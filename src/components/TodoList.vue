b1eb6a548ef32ad029ba141c4af3091f0ead0d47
igormacedo97 <igor.macedo097@gmail.com>
2022-01-13 22:12:28 -0300

    First commit - todoList Vue

---
+++ src/components/TodoList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="todo-list">
+    <h3>Todo List</h3>
+    <input
+      type="text"
+      class="input-new-item"
+      v-on:keyup.enter="addNewItemToList"
+    />
+
+    <ul>
+      <ListItem
+        v-for="(item, index) in list"
+        :key="index"
+        :item="item"
+        :index="index"
+        @delete-item="deleteItem"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from "./ListItem";
+
+export default {
+  name: "TodoList",
+  components: {
+    ListItem,
+  },
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    const itensInLocalStorage = JSON.parse(localStorage.getItem("list"));
+    this.list = itensInLocalStorage ? itensInLocalStorage : [];
+  },
+  methods: {
+    addNewItemToList(event) {
+      const newItem = event.target.value;
+      this.list.unshift({
+        label: newItem,
+        checked: false,
+      });
+
+      event.target.value = "";
+    },
+    deleteItem(index) {
+      this.list.splice(index, 1);
+    },
+    updateLocalStorage() {
+      localStorage.setItem("list", JSON.stringify(this.list));
+    },
+  },
+  watch: {
+    list() {
+      this.updateLocalStorage();
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todo-list {
+  width: 500px;
+  margin: auto;
+}
+
+.input-new-item {
+  width: 80%;
+  height: 35px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  width: 80%;
+  margin: 20px auto;
+
+  text-align: left;
+}
+</style>
